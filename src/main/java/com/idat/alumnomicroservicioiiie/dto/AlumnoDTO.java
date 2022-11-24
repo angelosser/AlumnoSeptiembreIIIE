47017a012a05c1323fcfacc40d14c01b675b4693
Angelo Cortes Baldovino <angelo.cortesb@gmail.com>
2022-11-23 20:58:54 -0500

    v3

---
+++ src/main/java/com/idat/alumnomicroservicioiiie/dto/AlumnoDTO.java
@@ -0,0 +1,11 @@
+package com.idat.alumnomicroservicioiiie.dto;
+
+import lombok.Data;
+
+@Data
+public class AlumnoDTO {
+
+    private Integer cod;
+    private String nom;
+    private String ape;
+}
